cf613bd0a25cb9375af3033ec9f1001f54e301d0
Optivat <67515969+Optivat@users.noreply.github.com>
2022-05-06 10:38:00 -0700

    pain

--- src/main/java/com/optivat/plugin/Entities/PlayerEntity.java
+++ src/main/java/com/optivat/plugin/Entities/PlayerEntity.java
@@ -9,57 +9,57 @@ import java.util.UUID;
 
 public class PlayerEntity {
 
-        private Player player;
-        private int maxHp;
-        private int currentHp;
-        private int regenRateHp;
-        private int defense;
-        private int strength;
-        private int damage;
-        private UUID uuid;
-        private Location loc;
-        private static Main main;
+    private Player player;
+    private int maxHp;
+    private int currentHp;
+    private int regenRateHp;
+    private int defense;
+    private int strength;
+    private int damage;
+    private UUID uuid;
+    private Location loc;
+    private static Main main;
 
-        public PlayerEntity(Main main) {
-            this.main = main;
-        }
-
-
-        public PlayerEntity(Player p) {
-            this.player = p;
-            maxHp = 100;
-            strength = 20;
-            damage = strength/4;
-            currentHp = maxHp;
-            regenRateHp = 5;
-            uuid = p.getUniqueId();
-            loc = p.getLocation();
-            defense = 0;
-
-        }
-        public Player getPlayer() { return player; }
-        public int getMaxHP() { return maxHp; }
-        public int getCurrentHP() { return currentHp; }
-        public int getRegenRateHp() { return regenRateHp; }
-        public int getDefense() { return defense; }
-        public int getStrength() { return strength; }
-        public int getDamage() { return damage; }
-        public UUID getUUID() { return uuid; }
-        public Location getLocation() { return loc; }
-
-        public void setCurrentHp(int chp) { currentHp = chp; }
-        public void setLocation(Location location) { loc = location; }
+    public PlayerEntity(Main main) {
+        this.main = main;
+    }
 
 
+    public PlayerEntity(Player p) {
+        this.player = p;
+        maxHp = 100;
+        strength = 20;
+        damage = strength/4;
+        currentHp = maxHp;
+        regenRateHp = 5;
+        uuid = p.getUniqueId();
+        loc = p.getLocation();
+        defense = 0;
+
+    }
+    public Player getPlayer() { return player; }
+    public int getMaxHP() { return maxHp; }
+    public int getCurrentHP() { return currentHp; }
+    public int getRegenRateHp() { return regenRateHp; }
+    public int getDefense() { return defense; }
+    public int getStrength() { return strength; }
+    public int getDamage() { return damage; }
+    public UUID getUUID() { return uuid; }
+    public Location getLocation() { return loc; }
+
+    public void setCurrentHp(int chp) { currentHp = chp; }
+    public void setLocation(Location location) { loc = location; }
 
 
-        public static PlayerEntity getPlayerEntity(UUID uuid) {
-            for(PlayerEntity p: main.totalPlayers) {
-                if (p.uuid == uuid) {
-                    return p;
-                }
+
+
+    public static PlayerEntity getPlayerEntity(UUID uuid) {
+        for(PlayerEntity p: main.totalPlayers) {
+            if (p.uuid == uuid) {
+                return p;
             }
-            return null;
         }
+        return null;
+    }
 
 }
